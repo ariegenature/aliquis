3a6363a1a7275d6f3815e48d916b1dd3e16c6663
Yg Versil <ygversil@openmailbox.org>
2017-08-15 19:57:23 +0200

    [vue] Add vee-validate dependency for Vue.js client
    And for now, disable automatic validation.

--- aliquisjs/src/main.js
+++ aliquisjs/src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import App from './App'
 import Buefy from 'buefy'
+import VeeValidate from 'vee-validate'
 import Vue from 'vue'
 import router from './router'
 import 'buefy/lib/buefy.css'
@@ -10,6 +11,9 @@ Vue.config.productionTip = false
 
 Vue.use(Buefy, {
   defaultIconPack: 'fa'
+})
+Vue.use(VeeValidate, {
+  events: ''
 })
 
 /* eslint-disable no-new */
