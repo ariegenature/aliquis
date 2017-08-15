cfd3cf8adaea552e4c2cdbec5a3bd2f928b4dee6
Yg Versil <ygversil@openmailbox.org>
2017-08-15 19:39:05 +0200

    [vue] Improve top-level HTML a little bit
    Use Bulma classes.

--- aliquisjs/src/App.vue
+++ aliquisjs/src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <header class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Your ANA account</h1>
+        </div>
+      </div>
+    </header>
+    <main class="section">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column">
+          <div class="box">
+            <router-view></router-view>
+          </div>
+        </div>
+      </div>
+    </div>
+    </main>
   </div>
 </template>
 
