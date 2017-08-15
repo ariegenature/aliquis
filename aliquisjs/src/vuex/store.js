56c5b5ca191a7df6f05f6675f851b1b55c795ff7
Yg Versil <ygversil@openmailbox.org>
2017-08-15 16:03:02 +0200

    [vue] Add simple store for a sign up form
    Using vuex gives us an error in test because of a missing polyfill. Importing `es6-promise/auto` fix
    this error.

---
+++ aliquisjs/src/vuex/store.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+  signUpFirstName: '',
+  signUpSurname: '',
+  signUpDisplayName: '',
+  signUpEmail: '',
+  signUpUsername: '',
+  signUpPassword: '',
+  emailRegExp: /^([a-zA-Z0-9.+-]+@[a-zA-Z\d-]+(\.[a-zA-Z\d-]+)+$)/,
+  usernameRegExp: /^[a-z][a-z0-9_.]+$/
+}
+
+export default new Vuex.Store({
+  state
+})
