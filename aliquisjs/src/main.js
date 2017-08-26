54c6ac0eea265718440e420cd7095e8734b2553f
Yg Versil <ygversil@openmailbox.org>
2017-08-26 21:12:33 +0200

    [vue] Localize vee-validate messages

--- aliquisjs/src/main.js
+++ aliquisjs/src/main.js
@@ -2,19 +2,26 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import App from './App'
 import Buefy from 'buefy'
-import VeeValidate from 'vee-validate'
+import VeeValidate, { Validator } from 'vee-validate'
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+import fr from 'vee-validate/dist/locale/fr'
 import router from './router'
 import 'buefy/lib/buefy.css'
+
+var locale = navigator.languages !== undefined ? navigator.languages[0] : navigator.language
+locale = locale.substring(0, 2)
 
 Vue.config.productionTip = false
 
 Vue.use(Buefy, {
   defaultIconPack: 'fa'
 })
+
+Validator.addLocale(fr)
 Vue.use(VeeValidate, {
-  events: ''
+  events: '',
+  locale: locale
 })
 Vue.use(VueResource)
 
