30b24643db39dcbf2dff2165d228353dd8579149
Yg Versil <ygversil@openmailbox.org>
2018-02-20 16:09:19 +0100

    [cli] Add 2 commands to install and build Vue.js client

--- aliquis/aliquis_cli.py
+++ aliquis/aliquis_cli.py
@@ -1,9 +1,13 @@
 """Aliquis command line commands."""
 
+import os
+import subprocess
+
 from babel.messages.frontend import CommandLineInterface as BabelCLI
 import click
 
 from aliquis import create_app, read_config
+import aliquis
 
 
 app = create_app(read_config())
@@ -58,3 +62,26 @@ def i18ncompile():
     BabelCLI().run(['', 'compile', '-d', 'aliquis/i18n/'])
     click.echo('-> Translations compiled.\n')
     click.echo('You should now restart Flask server to take new translations into account.')
+
+
+@app.cli.command(name='install-js-deps')
+def install_js_deps():
+    """Run ``npm install`` for the Vue.js client in order to install its JavaScript dependencies."""
+    click.echo('-> Installing JavaScript dependencies for the Vue.js client...')
+    subprocess.check_call(['npm',
+                           '--prefix={0}'.format(os.path.join(os.path.dirname(aliquis.__file__),
+                                                              'contributejs')),
+                           'install'])
+    click.echo('-> JavaScript dependencies succesfully installed.')
+
+
+@app.cli.command(name='build-js-client')
+def build_js_client():
+    """Execute ``npm run build`` for the Vue.js client to build it so that it can be served."""
+    click.echo('-> Building the Vue.js client...')
+    subprocess.check_call(['npm',
+                           '--prefix={0}'.format(os.path.join(os.path.dirname(aliquis.__file__),
+                                                              'contributejs')),
+                           'run',
+                           'build'])
+    click.echo('-> Vue.js client succesfully built.')
