4053f10c5946be3e30650e5a7ca20412c3715f67
Yg Versil <ygversil@openmailbox.org>
2018-02-21 10:03:08 +0100

    [cli] More robust i18n commands

--- aliquis/aliquis_cli.py
+++ aliquis/aliquis_cli.py
@@ -12,16 +12,18 @@ import aliquis
 
 app = create_app(read_config())
 
+I18N_PATH = os.path.join(os.path.dirname(aliquis.__file__), 'i18n')
+
 
 def _extract_18n_messages():
     """Extract messages to translate from application files."""
     BabelCLI().run(['', 'extract', '-F', 'babel.cfg', '-k', '_t', '--no-location', '--sort-output',
-                    '--omit-header', '-o', 'aliquis/i18n/messages.pot', 'aliquis'])
+                    '--omit-header', '-o', os.path.join(I18N_PATH, 'messages.pot'), 'aliquis'])
 
 
 def _write_message_files(lang, command='update'):
     """Extract messages to translate from application files."""
-    BabelCLI().run(['', command, '-i', 'aliquis/i18n/messages.pot', '-d', 'aliquis/i18n/',
+    BabelCLI().run(['', command, '-i', os.path.join(I18N_PATH, 'messages.pot'), '-d', I18N_PATH,
                     '-l', lang])
 
 
@@ -37,7 +39,7 @@ def i18ninit():
     click.echo('-> i18n message files initialized.')
     click.echo('You should now edit translations in following files:')
     for lang in langs:
-        click.echo('aliquis/i18n/{0}/LC_MESSAGES/messages.po'.format(lang))
+        click.echo(os.path.join(I18N_PATH, lang, 'LC_MESSAGES', 'messages.po'))
 
 
 @app.cli.command()
@@ -52,14 +54,14 @@ def i18nupdate():
     click.echo('-> i18n message files updated.\n')
     click.echo('You should now edit translations in following files:')
     for lang in langs:
-        click.echo('  * aliquis/i18n/{0}/LC_MESSAGES/messages.po'.format(lang))
+        click.echo(os.path.join(I18N_PATH, lang, 'LC_MESSAGES', 'messages.po'))
 
 
 @app.cli.command()
 def i18ncompile():
     """Compile translations for configured languages."""
     click.echo('-> Compiling translations...')
-    BabelCLI().run(['', 'compile', '-d', 'aliquis/i18n/'])
+    BabelCLI().run(['', 'compile', '-d', I18N_PATH])
     click.echo('-> Translations compiled.\n')
     click.echo('You should now restart Flask server to take new translations into account.')
 
